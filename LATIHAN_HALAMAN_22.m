b2fb88b9dece512ec1e39ef650516899e1ec9293
cxxuvy <mitra091202@gmail.com>
2021-08-22 00:19:51 +0800

    Latihan Soal : Halaman 22

---
+++ LATIHAN_HALAMAN_22.m
@@ -0,0 +1,51 @@
+%NOMOR 1
+12 / 3.5
+(3 + 5 / 4) ^ 2
+(0.25 ^ 2 + 0.75 ^ 2) ^ 1/2
+2 / (6 / 0.3)
+
+%NOMOR 2
+A = 25, B = 125, C = 89, D = 98
+X = A + B + C
+Y = A / (D + B)
+Z = D ^ A / B + C
+
+%NOMOR 3
+% luas (variabel valid)
+% kel_1 (variabel valid)
+% 2_data (variabel tidak valid)
+% diff:3 (variabel tidak valid)
+% Time (variabel tidak valid)
+% time_from_start (variabel valid)
+% 10_hasil_terakhir (variabel tidak valid)
+% nilai-awal (variabel tidak valid)
+
+%NOMOR 4
+x = pi / 6 ; y = 0.0001
+sqrt(y)
+exp(min(x))
+sin(x)
+cos(2 * x)
+tan(3 * x)
+log10(x)
+log2(y)
+log(y)
+
+%NOMOR 5
+p = 9 + 16i
+q = -9 + 16i
+
+r = p * q
+s = p / q
+p - r
+r + s
+p ^ 2
+sqrt(y)
+abs(p)
+angle(p)
+abs(q)
+angle(q)
+abs(r)
+angle(r)
+abs(s)
+angle(s)
